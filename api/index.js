3408dae94028d901aebf655ef39046573a4204d0
kaif <shaikhkaif3258@gmail.com>
2024-12-25 22:58:25 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+main()
+.then(()=>{
+    console.log("connect successful");
+})
+.catch((err) =>{
+     console.log(err)
+    }
+);
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/mern-auth');
+}
 
 app.listen(3000,()=>{
     console.log("server listening on port 3000");
